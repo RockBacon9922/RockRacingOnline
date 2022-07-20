d4f8eff33c05e0672de8175d44970c61f3ba58f9
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-07-20 22:32:59 +0000

    Revert "Started to work on the project again."
    This reverts commit 4a6a15ba89289ff908e59552ef3cc60c6f8da419.

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,2 +1,10 @@
 export default function handler(req, res) {
-	res.status(200).send("hello")
+	if (!req.query.length) {
+		req.query.length = 20;
+	}
+	const crypto = require("crypto");
+	var randomnumber = crypto
+		.randomBytes(parseInt(req.query.length))
+		.toString("hex");
+	res.status(200).json({ randomnumber });
+}
