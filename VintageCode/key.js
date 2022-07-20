fc51ad5f9470b25ecc0fe52fa37c77c549faa8f5
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-07-20 23:27:29 +0100

    Delete VintageCode directory

--- VintageCode/key.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-const publicKey = fs.readFileSync("publickey.pem", "utf8");
-
-export default function key(req, res) {
-	console.log("gotkey");
-	res.status(200).json({
-		key: publicKey,
-	});
-}
